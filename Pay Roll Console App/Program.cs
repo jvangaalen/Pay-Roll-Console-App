dfe43edfcc7c6f889da9f0b16531ebba3f0dd27c
Jerome vanGaalen <jerome.vangaalen@gmail.com>
2019-01-15 19:49:24 -0500

    Add project files.

---
+++ Pay Roll Console App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pay_Roll_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
